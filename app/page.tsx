6faf5d0d447c42a1b9cfbec04315440b3d2de326
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-11 16:28:05 -0600

    Beginning to work on the Catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+            <h1 className="text-4xl font-extrabold">
+                Vehicle Catalogue
+            </h1>
+            <p>
+                Explore the vehicles you might like
+            </p>
+        </div>
+      </div>
     </main>
   );
 }
