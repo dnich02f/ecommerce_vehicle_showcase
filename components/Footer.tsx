d7cb8882aaabd7452de61b2991e1038504e30a04
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-11 16:12:58 -0600

    added generic privacy policy and terms of use links with styling.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,6 +39,20 @@ const Footer = () => {
                     ))}
                 </div>
             </div>
+
+                <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+                    <p>
+                        @2024 Vehicle Hub. All Rights Reserved.
+                    </p>
+                    <div className="footer__copyrights-link">
+                        <Link href="/" className="text-gray-500">
+                            Privacy Policy
+                        </Link>
+                        <Link href="/" className="text-gray-500">
+                            Terms of Use
+                        </Link>
+                    </div>
+                </div>
         </footer>
     )
 }
