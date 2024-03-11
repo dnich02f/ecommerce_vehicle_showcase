d3ef1ba712bd2199c9e05992216d9c666e0efd1c
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-11 10:48:21 -0600

    Creating the Initial layout of the application, overriding default globals.css, and tailwind.config.ts. Created a index.ts page to host exports and created the main Hero component of the app.

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, Book, or Rent a Vehicle -- quickly and easily
+                </h1>
+                <p className="hero__subtitle">
+                    Streamline your vehicle rental experience with our effortless booking experience.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
