2662ebb5797ac168dd184fac56a78d934c1e83de
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-12 16:00:52 -0600

    adding more styling to the carcard and a default image for now.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,14 +2,41 @@
 import React, { useState } from 'react';
 import {CarProps} from "@/types";
 import Image from "next/image";
+import CustomButton from "./CustomButton";
+import {calculateCarRent} from "@/utils";
 
 interface CarCardProps {
     car: CarProps
 }
 
-const CarCard = (car: CarCardProps) => {
+const CarCard = ({car}: CarCardProps) => {
+    // destructure car properties
+    const { city_mpg, year, make, model,
+        transmission, drive } = car
+
+    const carRent = calculateCarRent(city_mpg, year)
+
     return (
-        <div></div>
+        <div className={"car-card group"}>
+            <div className={"car-card__content"}>
+                <h2 className={"car-card__content-title"}>
+                    {make} {model}
+                </h2>
+            </div>
+            <p className={"flex mt-6 text-[32px] font-extrabold"}>
+                <span className={"self-start text-[14px] font-semibold"}>
+                    $
+                </span>
+                {carRent}
+                <span className={"self-end text-[14px] font-medium"}>
+                    /day
+                </span>
+            </p>
+            <div className={"relative w-full h-40 my-3 object-contain"}>
+                <Image src={"/hero.png"} alt={"car model"}
+                fill priority className={"object-contain"}/>
+            </div>
+        </div>
     )
 }
 
