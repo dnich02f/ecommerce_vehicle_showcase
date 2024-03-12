0d1512e5614c31e480a850ccb4dbf80d1e221600
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-12 14:46:48 -0600

    made the Combobox.Options className relative instead of absolute. The list dropdown was appearing behind the CustomFilters.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,7 +5,7 @@ import { Combobox, Transition } from "@headlessui/react";
 import Image from 'next/image';
 import {manufacturers} from "@/constants";
 
-const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
 
   const filteredManufacturers =
@@ -20,7 +20,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManuFacturer}>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           {/* Button for the combobox. Click on the icon to see the complete dropdown */}
           <Combobox.Button className='absolute top-[14px]'>
@@ -50,17 +50,10 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              className='relative mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
             >
-              {filteredManufacturers.length === 0 && query !== "" ? (
-                <Combobox.Option
-                  value={query}
-                  className='search-manufacturer__option'
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ) : (
+              {
                 filteredManufacturers.map((item) => (
                   <Combobox.Option
                     key={item}
@@ -71,22 +64,10 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
                     }
                     value={item}
                   >
-                    {({ selected, active }) => (
-                      <>
-                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                          {item}
-                        </span>
-
-                        {/* Show an active blue background color if the option is selected */}
-                        {selected ? (
-                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
-                          ></span>
-                        ) : null}
-                      </>
-                    )}
+                    {item}
                   </Combobox.Option>
                 ))
-              )}
+              }
             </Combobox.Options>
           </Transition>
         </div>
