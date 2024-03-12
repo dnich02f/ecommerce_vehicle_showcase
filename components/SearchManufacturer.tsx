c7531f5a143ae550bea127773710fa2841da666c
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-12 15:17:59 -0600

    Finished the SearchManufacturer component.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -64,7 +64,19 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     }
                     value={item}
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
+                          {item}
+                        </span>
+
+                        {/* Show an active blue background color if the option is selected */}
+                        {selected ? (
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
+                          ></span>
+                        ) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 ))
               }
