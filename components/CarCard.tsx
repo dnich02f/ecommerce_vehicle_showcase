3f51335e29e5fde9964948805f640fa4af65f17c
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-12 15:48:46 -0600

    finished setting up the car props.

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {CarCardProps} from "@/types";
+"use client"
+import React, { useState } from 'react';
+import {CarProps} from "@/types";
+import Image from "next/image";
+
+interface CarCardProps {
+    car: CarProps
+}
 
 const CarCard = (car: CarCardProps) => {
     return (
--- types/index.ts
+++ types/index.ts
@@ -13,6 +13,17 @@ export interface SearchManufacturerProps {
 }
 
 
-export interface CarCardProps {
-    car?: string,
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
