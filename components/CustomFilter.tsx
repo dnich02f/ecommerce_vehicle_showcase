3fedc6ab83562b8d8c8612d1560a35d0e1e38900
Darryl Nichols <darryl.nichols@simfoods.com>
2024-03-13 15:13:55 -0600

    finishing client side

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,14 +1,12 @@
 "use client"
 import React, { useState, Fragment } from 'react';
 import Image from 'next/image';
-import {useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import {CustomFilterProps} from "@/types";
-import {updateSearchParams} from "@/utils";
 
 const CustomFilter = ({title, options, setFilter}: CustomFilterProps) => {
-    const [selected, setSelected] = useState(options[0])
-
+    const [selected, setSelected]
+        = useState(options[0])
 
     return (
         <div className={"w-fit"}>
@@ -16,7 +14,7 @@ const CustomFilter = ({title, options, setFilter}: CustomFilterProps) => {
                 value={selected}
                 onChange={(e) => {
                 setSelected(e);
-                setFilter(e);
+                setFilter(e.value);
                 }}
             >
                 <div className={"relative w-fit z-10"}>
